86800e001bd0e81d9220e665a81620605ebb9def
AnKhudyakov <andrew_job@list.ru>
2023-04-25 00:12:21 +0400

    feat: add burger active

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+const burgerMenuButton = document.getElementById("burger-menu-button");
+const navigationMenu = document.getElementById("navigation-menu");
+
+function myFunction() {
+  burgerMenuButton.classList.toggle("open");
+  navigationMenu.classList.toggle("open");
+}
